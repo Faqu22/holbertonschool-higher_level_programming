89f4e0d00e39fa5717b18eb1ae685e1fc50bf1b8
Faqu22 <facundoalvarez415@gmail.com>
2023-06-01 10:32:10 -0300

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-count = 0
-print("{}{}".format(len(argv) - 1,
-                    " arguments:" if len(argv) > 1 else " arguments."))
-for x in argv:
-    if count != 0:
-        print("{}: {}".format(count, x))
-    count = count + 1
+if __name__ == "__main__":
+    from sys import argv
+    count = 0
+    print("{}{}".format(len(argv) - 1,
+                        " arguments:" if len(argv) > 1 else " arguments."))
+    for x in argv:
+        if count != 0:
+            print("{}: {}".format(count, x))
+        count = count + 1
