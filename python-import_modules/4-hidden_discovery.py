837054d7c5eb05b05930c57fa7b485b79eedbeda
Faqu22 <facundoalvarez415@gmail.com>
2023-06-01 20:39:23 -0300

    Task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    for x in dir("hidden_4.pyc"):
+    import hidden_4
+    for x in dir(hidden_4):
         if x.startswith("__") is False:
             print(x)
