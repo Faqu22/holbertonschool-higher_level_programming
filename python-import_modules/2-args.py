972d40f3e70221a55d8c70bef429a0d05554ac07
Faqu22 <facundoalvarez415@gmail.com>
2023-06-01 10:50:55 -0300

    Low Fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     from sys import argv
     count = 0
-    print("{}{}".format(len(argv) - 1,
-                        " arguments:" if len(argv) > 1 else " arguments."))
+    print("{} {}".format(len(argv) - 1,
+                         "arguments:" if len(argv) > 1 else "arguments."))
     for x in argv:
         if count != 0:
             print("{}: {}".format(count, x))
-        count = count + 1
+        count += 1
