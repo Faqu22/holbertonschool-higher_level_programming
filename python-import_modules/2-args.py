84c4683e78634ea2b5ee7a28424b3cbe3c928063
Faqu22 <facundoalvarez415@gmail.com>
2023-06-01 10:07:39 -0300

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 count = 0
-if len(argv) < 2:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(len(argv)))
-    for x in argv:
-        if count != 0:
-            print("{}: {}".format(count, x))
-        count = count + 1
+print("{}{}".format(len(argv) - 1,
+                    " arguments:" if len(argv) > 1 else " arguments."))
+for x in argv:
+    if count != 0:
+        print("{}: {}".format(count, x))
+    count = count + 1
