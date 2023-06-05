db9747e355603412850336b4792c4aadf573d221
Faqu22 <facundoalvarez415@gmail.com>
2023-06-05 09:56:48 -0300

    Task 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    str = "{}"
-    for x in range(0, len(my_list)):
-        print(str.format(my_list[x]))
+    for x in my_list):
+        print("{}".format(x))
