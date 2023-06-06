3143aedcf5881b309ad7398e697a4bf2c3866808
Faqu22 <facundoalvarez415@gmail.com>
2023-06-06 14:17:50 -0300

    Task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if my_list:
         y = 0
         for x in my_list:
-            if x > y:
+            if x >= y:
                 y = x
         return y
     return None
