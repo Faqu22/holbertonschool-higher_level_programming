517026bfe5082605833163f2c80ba59f0de7fb43
Faqu22 <facundoalvarez415@gmail.com>
2023-06-07 16:27:54 -0300

    Task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,10 +2,5 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    x = ""
-    i = 0
-    for y in a_dictionary:
-        if a_dictionary[y] > i:
-            i = a_dictionary[y]
-            x = y
-    return x
+    else:
+        return max(a_dictionary, key=lambda x: a_dictionary[x])
