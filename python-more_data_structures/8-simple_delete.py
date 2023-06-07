9b796a81b2cd22ced6070f01758cb803bc429ca8
Faqu22 <facundoalvarez415@gmail.com>
2023-06-07 16:09:51 -0300

    Task 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    try:
-        a_dictionary.pop(key)
-    except a_dictionary[key].DoesNotExist:
-        pass
+    if key != "":
+        try:
+            a_dictionary.pop(key)
+        except a_dictionary[key].DoesNotExist:
+            pass
     return a_dictionary
