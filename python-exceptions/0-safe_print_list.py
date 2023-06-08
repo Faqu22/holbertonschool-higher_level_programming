dc7cb965482519e05235b7602d520abc9a32c2e8
Faqu22 <facundoalvarez415@gmail.com>
2023-06-08 09:48:57 -0300

    Task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,8 @@ def safe_print_list(my_list=[], x=0):
     for y in range(0, x):
         try:
             print(my_list[y], end="")
+            i += 1
         except IndexError:
             break
     print()
-    return y
+    return i
