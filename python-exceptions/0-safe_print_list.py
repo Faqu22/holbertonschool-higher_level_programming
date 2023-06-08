53eeb5361654bacb890f19f91f4487b8ecb470a8
Faqu22 <facundoalvarez415@gmail.com>
2023-06-08 09:34:13 -0300

    Task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    for y in my_list:
-        print(y, end="")
-        i += 1
-        if x <= i:
+    for y in range(0, x):
+        try:
+            print(my_list[y], end="")
+            i += 1
+        except:
             break
     print()
     return i
