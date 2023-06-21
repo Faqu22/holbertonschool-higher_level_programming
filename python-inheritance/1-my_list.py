dea78742d1a169bafb1f16917fad0051a9f43988
Faqu22 <facundoalvarez415@gmail.com>
2023-06-21 10:51:59 -0300

    Task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,9 +5,6 @@
 class MyList(list):
     """class of an List"""
 
-    def __init__(self):
-        self = []
-
     def print_sorted(self):
         """print the list in the ascending sort"""
         print(sorted(self))
