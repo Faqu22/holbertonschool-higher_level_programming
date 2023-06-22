876834ccb34a33b38e9dfad4de65ef6182edff74
Faqu22 <facundoalvarez415@gmail.com>
2023-06-22 14:38:54 -0300

    Task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,11 +3,12 @@
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """class Rectangle"""
     def __init__(self, width, height):
         """Method init"""
-        self.integer_validator("width", width)
-        self.integer_validator("heigth", height)
+        self.integer_validator(width)
+        self.integer_validator(height)
         self.__width = width
         self.__height = height
