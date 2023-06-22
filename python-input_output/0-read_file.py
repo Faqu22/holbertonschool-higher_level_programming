ad400b7f9e7e7184e4f63e22c1eac9eed901035e
Faqu22 <facundoalvarez415@gmail.com>
2023-06-22 15:40:21 -0300

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """ Function that reads a text file (UTF8) and prints it"""
-    x = open(filename, "r")
-    print(x.read())
+    with open(filename, "r") as x:
+        print(x.read())
