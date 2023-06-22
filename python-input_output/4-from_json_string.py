1f8d47b3e1316f519f4b85b81e282c3fe7c027f8
Faqu22 <facundoalvarez415@gmail.com>
2023-06-22 18:01:32 -0300

    Task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -3,7 +3,7 @@
 import json
 
 
-def to_json_string(my_obj):
+def from_json_string(my_str):
     """Function that returns an object (Python data structure)\
         represented by a JSON string"""
-    return json.loads(my_obj)
+    return json.loads(my_str)
