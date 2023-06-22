b534ddd451eb1512bfc56337192597b55419ef2d
Faqu22 <facundoalvarez415@gmail.com>
2023-06-22 14:35:46 -0300

    Tas 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-""" 6-base_geometry.py Module"""
+""" 8-rectangle Module"""
 
-
-class BaseGeometry:
-    """class BaseGeometry"""
-    def area(self):
-        """Method what raised the exc with an msg"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Method that validates a value is or not is a int"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
     """class Rectangle"""
