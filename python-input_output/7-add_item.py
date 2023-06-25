5f9acbe28f350ce3241eb8c7cd88ace36b8b45e8
Faqu22 <facundoalvarez415@gmail.com>
2023-06-25 14:38:10 -0300

    Task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Task 7"""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = sys.argv[:1]
+
+try:
+    with open('add_item.json') as x:
+        data = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    data = []
+
+data.extend(args)
+save_to_json_file(data, 'add_item.json')
