798fb203af446ac9154e43cf122ff58d82635f0e
Faqu22 <facundoalvarez415@gmail.com>
2023-06-27 01:11:05 -0300

    Task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
         """
         That method returns the JSON string representation of list_dictionaries
         """
-        if list_dictionaries is None or list_dictionaries == []:
-            return []
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
