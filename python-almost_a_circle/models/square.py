f04af04998f2ba7d90ca01fde8f0a4d6f977cdc2
Faqu22 <facundoalvarez415@gmail.com>
2023-06-27 00:30:13 -0300

    Task 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,6 +10,18 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(height=size, width=size, x=x, y=y, id=id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.width = value
+
     def __str__(self):
         """
         This function is called when you want to print the class
