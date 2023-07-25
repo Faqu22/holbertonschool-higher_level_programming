2be3776b8632415ef8f4c366b41a2243d3a90ad0
Faqu22 <facundoalvarez415@gmail.com>
2023-07-25 16:43:46 -0300

    task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,5 @@
 -- This script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT title, genre_id FROM tv_shows, tv_show_genres ORDER BY title, genre_id;
+SELECT title, genre_id
+FROM tv_shows, tv_show_genres
+GROUP BY title, genre_id
+ORDER BY title;
