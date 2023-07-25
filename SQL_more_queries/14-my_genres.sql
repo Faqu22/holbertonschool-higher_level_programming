2eed042d74427234683e1aeb384c17a5f74fa41a
Faqu22 <facundoalvarez415@gmail.com>
2023-07-25 19:00:35 -0300

    task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,5 +3,7 @@ SELECT name
 FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = 8
+LEFT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY name ASC;
