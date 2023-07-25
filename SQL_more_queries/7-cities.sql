1520242426add849ed393c29fa11cc2a52556ed5
Faqu22 <facundoalvarez415@gmail.com>
2023-07-25 12:56:36 -0300

    task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,6 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE table if not EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+    state_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (state_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL
 );
