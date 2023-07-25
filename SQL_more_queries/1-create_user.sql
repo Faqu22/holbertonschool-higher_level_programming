657959062103d3b79d4aa1549b621e3412a0db71
Faqu22 <facundoalvarez415@gmail.com>
2023-07-25 12:02:46 -0300

    task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- This script that creates the MySQL server user user_0d_1.
 -- Task 1
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL ON `database`.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
