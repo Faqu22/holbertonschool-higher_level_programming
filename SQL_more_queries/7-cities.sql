acf6e500bc0910d7eced9bdc7f3eb356e1c424ba
Faqu22 <facundoalvarez415@gmail.com>
2023-07-25 12:57:43 -0300

    task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- This script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa).
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE table if not EXISTS hbtn_0d_usa.cities (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY (state_id) REFERENCES states(id),
