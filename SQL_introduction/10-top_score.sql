3ba9c54f5950feec4314103a94f9ee504277cc7e
Faqu22 <facundoalvarez415@gmail.com>
2023-07-25 09:29:40 -0300

    task 10

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,4 @@
 -- This script that lists all records of the table second_table.
-SELECT name, Sum(score)
+SELECT name, score
 FROM second_table
-Group By name
-Order By Sum(score) Desc;
+Order By score Desc;
