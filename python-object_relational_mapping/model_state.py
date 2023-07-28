a744d2b82a3dd54bb846b0d0a8ac02d628716253
Faqu22 <facundoalvarez415@gmail.com>
2023-07-28 10:18:01 -0300

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-import MySQLdb
-import sys
 """
     Representation of Table States.
 """
 Base = declarative_base()
-db = MySQLdb.connect(host='localhost', port=3306,
-                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
 
 class State(Base):
-    "State class"
+    """
+    State class
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
