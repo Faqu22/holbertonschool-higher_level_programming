7ac3c02a10c4322dd0d749833b640cf2cf135bc5
Faqu22 <facundoalvarez415@gmail.com>
2023-07-28 10:46:43 -0300

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Write a script that prints the first \
-    State object from the database hbtn_0e_6_usa
+    Write a script that prints the first \
+        State object from the database hbtn_0e_6_usa
 """
 from model_state import Base, State
 from sqlalchemy import (create_engine)
