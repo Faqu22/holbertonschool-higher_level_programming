b518c1c165e97913cd6731583f3c94edd4db1063
Faqu22 <facundoalvarez415@gmail.com>
2023-07-28 10:15:45 -0300

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,7 +7,8 @@ import sys
     Representation of Table States.
 """
 Base = declarative_base()
-
+db = MySQLdb.connect(host='localhost', port=3306,
+                     user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
 class State(Base):
     "State class"
